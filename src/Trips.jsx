915b4f2555ef8b9da0360fd585cda1be789ec5b5
MartinGV10 <mganen10@gmail.com>
2025-12-05 22:59:15 -0500

    Finished trips page

--- src/Trips.jsx
+++ src/Trips.jsx
@@ -1,4 +1,7 @@
 import './css/Trips.css'
+import sarasota from './assets/sara.jpg'
+import tampa from './assets/tampa.jpg'
+import fm from './assets/fm.jpg'
 
 function Trips() {
     return (
@@ -9,30 +12,30 @@ function Trips() {
                 <h2>Nothing is better than going away with you!</h2>
                 <div className="trip-items">
                     <div className="t-item">
-                        <img src="" alt="plcaeholder" />
+                        <img src={sarasota} alt="plcaeholder" className='item-img'/>
                         <div className="t-top">
                             <h3 className="t-title">Sarasota</h3>
                             <p className="t-date"><i>July 2025</i></p>
                         </div>
-                        <p className="t-desc">We went here</p>
+                        <p className="t-desc">Our first trip after not seeing each other for over a month</p>
                     </div>
                     
                     <div className="t-item">
-                        <img src="" alt="plcaeholder" />
+                        <img src={tampa} alt="plcaeholder" className='item-img'/>
                         <div className="t-top">
-                            <h3 className="t-title">Sarasota</h3>
-                            <p className="t-date"><i>July 2025</i></p>
+                            <h3 className="t-title">Tampa/St-Pete</h3>
+                            <p className="t-date"><i>August 2025</i></p>
                         </div>
-                        <p className="t-desc">We went here</p>
+                        <p className="t-desc">Second trip (basically back home lol) for your birthday!</p>
                     </div>
 
                     <div className="t-item">
-                        <img src="" alt="plcaeholder" />
+                        <img src={fm} alt="plcaeholder" className='item-img'/>
                         <div className="t-top">
-                            <h3 className="t-title">Sarasota</h3>
+                            <h3 className="t-title">Fort Myers</h3>
                             <p className="t-date"><i>July 2025</i></p>
                         </div>
-                        <p className="t-desc">We went here</p>
+                        <p className="t-desc">Last time getting together in the summer before school</p>
                     </div>
                     
                 </div>
